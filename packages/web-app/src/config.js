c441cee56be487a3a32d9fb86936c96e4ba637e5
Nicola Molinari <nicola.molinari@commercetools.de>
2017-11-13 16:22:11 +0100

    chore(app): use a different port

--- packages/web-app/src/config.js
+++ packages/web-app/src/config.js
@@ -1,14 +1,14 @@
 export const APP_CONFIG = {
-  url: 'http://localhost:3000',
+  url: 'http://localhost:4000',
 };
 
 export const AUTH_CONFIG = {
   domain: 'tourname.eu.auth0.com',
   clientId: 'ZpxENtfYcVeKgokEz5WkNpyy8PQC8XmN',
-  callbackUrl: 'http://localhost:3000/auth/callback',
+  callbackUrl: 'http://localhost:4000/auth/callback',
   apiUrl: 'https://tourname.eu.auth0.com/api/v2/',
 };
 
 export const GRAPHQL_CONFIG = {
-  url: 'http://localhost:3001/graphql',
+  url: 'http://localhost:3999/graphql',
 };
