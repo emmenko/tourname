bc79388099ba9d6b051b2b467e296c19113bd02d
Nicola Molinari <nicola.molinari@commercetools.de>
2017-12-06 21:55:18 +0100

    refactor(app): define Root component

--- packages/web-app/src/index.js
+++ packages/web-app/src/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Application from './components/application';
-import AuthorizeCallback from './components/authorize-callback';
-import AuthorizeCallbackSilent from './components/authorize-callback-silent';
+import Root from './components/root';
 // import registerServiceWorker from './registerServiceWorker';
-
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route
-        exact={true}
-        path="/auth/callback/silent"
-        component={AuthorizeCallbackSilent}
-      />
-      <Route exact={true} path="/auth/callback" component={AuthorizeCallback} />
-      <Route component={Application} />
-    </Switch>
-  </Router>
-);
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 // registerServiceWorker();
