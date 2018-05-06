f1a7d6a23a9f8074280cd2773413073c9ac1e809
Nicola Molinari <nicola.molinari@commercetools.de>
2018-05-06 02:01:28 +0200

    fix(graphql): make base schema work

--- packages/graphql-server/src/index.js
+++ packages/graphql-server/src/index.js
@@ -2,17 +2,49 @@ const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
 
 const resolvers = {
-  // Query: {
-  //   feed(parent, args, ctx, info) {
-  //     return ctx.db.query.posts({ where: { isPublished: true } }, info);
-  //   },
-  //   drafts(parent, args, ctx, info) {
-  //     return ctx.db.query.posts({ where: { isPublished: false } }, info);
-  //   },
-  //   post(parent, { id }, ctx, info) {
-  //     return ctx.db.query.post({ where: { id } }, info);
-  //   },
-  // },
+  Query: {
+    // me: (parent, args, ctx, info) => {
+    //   return ctx.db.query.posts({ where: { isPublished: true } }, info);
+    // },
+    isOrganizationKeyUsed: async (parent, args, context) => {
+      // NOTE: using `context.db.exists.Organization({ key: args.key })`
+      // does not work as the internal query `organizations` requires
+      // a sub selection.
+      //   Error: Field 'organizations' of type 'Organization' must have a sub selection. (line 2, column 3))
+      const org = await context.db.query.organization(
+        { where: { key: args.key } },
+        '{ key }'
+      );
+      return Boolean(org);
+    },
+    organization: (parent, args, context, info) =>
+      context.db.query.organization({ where: { key: args.key } }, info),
+  },
+  Organization: {
+    // members: async (parent, args, context) => {
+    //   const normalizedUsers = parent.users.reduce((acc, user) => {
+    //     acc[user.auth0Id] = user;
+    //     return acc;
+    //   }, {});
+    //   const docs = await context.loaders.users.loadMany(
+    //     Object.keys(normalizedUsers)
+    //   );
+    //   return docs.map(doc =>
+    //     Object.assign({}, doc, {
+    //       role: normalizedUsers[doc.user_id].role, // The `user_id` field comes from auth0
+    //     })
+    //   );
+    // },
+    tournaments: (parent, args, context) =>
+      context.db.query.tournaments({
+        first: args.perPage,
+        skip: args.page > 0 ? (args.page - 1) * args.perPage : 0,
+        orderBy: `${args.sort.key}_${args.sort.order.toUpperCase()}`,
+        where: {
+          status_in: args.status || [],
+        },
+      }),
+  },
   // Mutation: {
   //   createDraft(parent, { title, text }, ctx, info) {
   //     return ctx.db.mutation.createPost(
@@ -54,6 +86,8 @@ const server = new GraphQLServer({
     }),
 });
 
-server.start(() =>
-  console.log(`Server is running on http://localhost:${process.env.APP_PORT}`)
+const port = process.env.APP_PORT;
+
+server.start({ port }, () =>
+  console.log(`Server is running on http://localhost:${port}`)
 );
