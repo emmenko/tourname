ffc415ecdbd0420abcaef70cae0b0a3f88d20d84
Nicola Molinari <nicola.molinari@commercetools.de>
2018-05-06 22:26:35 +0200

    refactor(graphql-server): remodel member refs, fix db queries/mutations

--- packages/graphql-server/src/index.js
+++ packages/graphql-server/src/index.js
@@ -26,40 +26,52 @@ const resolvers = {
       );
       return Boolean(org);
     },
-    organization: (parent, args, context, info) =>
-      context.db.query.organization({ where: { key: args.key } }, info),
+    organization: async (parent, args, context) => {
+      const orgs = await context.db.query.organizations({
+        where: {
+          AND: [
+            { key: args.key },
+            { memberRefs_every: { auth0Id: context.userId } },
+          ],
+        },
+        first: 1,
+      });
+      if (orgs && orgs.length > 0) return orgs[0];
+      // TODO: return proper status code
+      throw new Error(`Organization with key "${args.key}" not found`);
+    },
   },
   User: {
     id: parent => parent.user_id, // The `user_id` field comes from auth0
     createdAt: parent => parent.created_at,
     updatedAt: parent => parent.updated_at,
-    role: async (parent, args, context) => {
-      if (parent.role) return parent.role;
-      const userRef = await context.db.query.userRef({
-        where: { auth0Id: context.userId },
-      });
-      return userRef.role;
-    },
     availableOrganizations: (parent, args, context) =>
       context.db.query.organizations({
         orderBy: 'name_ASC',
-        where: { users_every: { auth0Id: context.userId } },
+        where: {
+          memberRefs_every: { auth0Id: context.userId },
+        },
       }),
   },
   Organization: {
-    users: async (parent, args, context) => {
-      const normalizedUsers = parent.users.reduce((acc, user) => {
-        acc[user.auth0Id] = user;
-        return acc;
-      }, {});
+    members: async (parent, args, context) => {
+      const normalizedMemberRefs = parent.memberRefs.reduce(
+        (acc, memberRef) => {
+          acc[memberRef.auth0Id] = memberRef;
+          return acc;
+        },
+        {}
+      );
       const docs = await context.loaders.users.loadMany(
-        Object.keys(normalizedUsers)
+        Object.keys(normalizedMemberRefs)
       );
-      return docs.map(doc =>
-        Object.assign({}, doc, {
-          role: normalizedUsers[doc.user_id].role, // The `user_id` field comes from auth0
-        })
-      );
+      return docs.map(doc => ({
+        id: doc.user_id,
+        email: doc.email,
+        name: doc.name,
+        picture: doc.picture,
+        role: normalizedMemberRefs[doc.user_id].role,
+      }));
     },
     tournaments: (parent, args, context) =>
       context.db.query.tournaments({
@@ -71,31 +83,197 @@ const resolvers = {
         },
       }),
   },
-  // Mutation: {
-  //   createDraft(parent, { title, text }, ctx, info) {
-  //     return ctx.db.mutation.createPost(
-  //       {
-  //         data: {
-  //           title,
-  //           text,
-  //         },
-  //       },
-  //       info
-  //     );
-  //   },
-  //   deletePost(parent, { id }, ctx, info) {
-  //     return ctx.db.mutation.deletePost({ where: { id } }, info);
-  //   },
-  //   publish(parent, { id }, ctx, info) {
-  //     return ctx.db.mutation.updatePost(
-  //       {
-  //         where: { id },
-  //         data: { isPublished: true },
-  //       },
-  //       info
-  //     );
-  //   },
-  // },
+  Mutation: {
+    /**
+     * Args:
+     * - key
+     * - name
+     */
+    createOrganization: async (parent, args, context) => {
+      const existingOrgForGivenKey = await context.db.query.organization({
+        where: { key: args.key },
+      });
+      if (existingOrgForGivenKey)
+        throw new Error(`An organization for key "${args.key}" already exist`);
+
+      return context.db.mutation.createOrganization({
+        data: {
+          key: args.key,
+          name: args.name,
+          memberRefs: {
+            create: [{ auth0Id: context.userId, role: 'Admin' }],
+          },
+          // tournaments: [],
+          // tournamentMatches: [],
+          // singleMatches: [],
+        },
+      });
+    },
+    // TODO: allow to remove an organization.
+    // - check that needs to be cleaned up
+    // removeOrganization
+    /**
+     * Only admin users can set other users as admin.
+     *
+     * Args:
+     * - organizationKey
+     * - memberId
+     */
+    promoteMemberToAdmin: async (parent, args, context) => {
+      if (context.userId === args.memberId)
+        throw new Error(
+          `You cannot set yourself admin of the organization "${
+            args.organizationKey
+          }"`
+        );
+
+      // Check that the user has access to the given organization
+      // and is an admin
+      const organizationResults = await context.db.query.organizations({
+        where: {
+          AND: [
+            { key: args.organizationKey },
+            {
+              memberRefs_every: {
+                AND: [{ auth0Id: context.userId }, { role: 'Admin' }],
+              },
+            },
+          ],
+        },
+      });
+      if (!organizationResults || organizationResults.length === 0)
+        // TODO: return proper status code
+        throw new Error(
+          `The organization with key "${
+            args.organizationKey
+          }" is either not found or you are not an Admin of such organization. In case you are part of the organization, remember that only admins can promote users to admin.`
+        );
+
+      // TODO: find a better way to check if the user exists in auth0
+      const targetUserDoc = await context.loaders.users.load(args.memberId);
+      if (!targetUserDoc)
+        throw new Error(
+          `The user "${
+            args.memberId
+          }" that you are trying to promote to admin does not exist`
+        );
+
+      // Check if the `memberId` is alredy Admin
+      const memberAdminResult = await context.db.memberRefs({
+        where: {
+          AND: [
+            { auth0Id: args.memberId },
+            { role: 'Admin' },
+            { organization: { key: args.organizationKey } },
+          ],
+        },
+      });
+      if (memberAdminResult && memberAdminResult.length > 0)
+        throw new Error(
+          `The member "${
+            args.memberId
+          }" is already an Admin of the organization "${args.organizationKey}"`
+        );
+
+      await context.db.mutation.updateManyMemberRefs({
+        where: {
+          AND: [
+            { auth0Id: args.memberId },
+            { organization: { key: args.organizationKey } },
+          ],
+        },
+        data: {
+          role: 'Admin',
+        },
+      });
+
+      // Return optimistic update
+      return Object.assign({}, memberAdminResult[0], {
+        role: 'Admin',
+      });
+    },
+    /**
+     * Args:
+     * - organizationKey
+     * - memberId
+     */
+    // addMemberToOrganization: async (parent, args, context) => {
+    //   // Check that the user has access to the given organization
+    //   const organizationDoc = await context.loaders.organizations.load(
+    //     args.organizationKey
+    //   );
+    //   if (!organizationDoc)
+    //     // TODO: return proper status code
+    //     throw new Error('Unauthorized');
+
+    //   // TODO: find a better way to check if the user exists in auth0
+    //   const targetUserDoc = await context.loaders.users.load(args.userId);
+    //   if (!targetUserDoc)
+    //     throw new Error(
+    //       `The user "${args.userId}" that you are trying to add does not exist`
+    //     );
+
+    //   const isoDate = new Date().toISOString();
+    //   await context.db.organizations.updateOne(
+    //     { _id: args.organizationKey },
+    //     {
+    //       $set: { lastModifiedAt: isoDate },
+    //       $addToSet: { users: { id: args.userId, isAdmin: false } },
+    //     }
+    //   );
+    //   context.loaders.organizations
+    //     .clear(args.organizationKey)
+    //     .load(args.organizationKey);
+    // },
+    /**
+     * Only admin users can remove users from an organization.
+     *
+     * Args:
+     * - organizationKey
+     * - userId
+     */
+    // removeUserFromOrganization: async (parent, args, context) => {
+    //   // Only admin users can remove users from the organization
+    //   // TODO: validate that current user is admin.
+    //   // TODO: current user cannot remove itself, instead the org should be removed
+    //   if (context.userId === args.userId)
+    //     throw new Error(
+    //       `You cannot remove yourself from the organization "${
+    //         args.organizationKey
+    //       }"`
+    //     );
+
+    //   // Check that the user has access to the given organization
+    //   const organizationDoc = await context.loaders.organizations.load(
+    //     args.organizationKey
+    //   );
+    //   if (!organizationDoc)
+    //     // TODO: return proper status code
+    //     throw new Error('Unauthorized');
+
+    //   const userSelfInOrg = organizationDoc.users.find(
+    //     user => user.id === context.userId
+    //   );
+    //   if (!userSelfInOrg.isAdmin)
+    //     throw new Error(
+    //       `You are not an admin of the organization "${
+    //         args.organizationKey
+    //       }". Only admins can remove users`
+    //     );
+
+    //   const isoDate = new Date().toISOString();
+    //   await context.db.organizations.updateOne(
+    //     { _id: args.organizationKey },
+    //     {
+    //       $set: { lastModifiedAt: isoDate },
+    //       $pull: { users: { id: args.userId } },
+    //     }
+    //   );
+    //   context.loaders.organizations
+    //     .clear(args.organizationKey)
+    //     .load(args.organizationKey);
+    // },
+  },
 };
 
 const checkJwt = jwt({
