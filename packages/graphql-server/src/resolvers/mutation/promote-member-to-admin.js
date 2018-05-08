0b0f7b60ec70f7fc758161a7b52f220a47533b75
Nicola Molinari <nicola.molinari@commercetools.de>
2018-05-08 09:07:28 +0200

    refactor(graphql-server): extract mutation validations into reusable functions

--- packages/graphql-server/src/resolvers/mutation/promote-member-to-admin.js
+++ packages/graphql-server/src/resolvers/mutation/promote-member-to-admin.js
@@ -1,41 +1,14 @@
-/**
- * Only admin members can set other members as admin.
- *
- * Args:
- * - organizationKey
- * - memberId
- */
-module.exports = async (parent, args, context, info) => {
+const isUserAdminOfOrganization = require('../../validations/is-user-admin-of-organization');
+
+const isTargetMemberNotSelf = (args, context) => {
   if (context.userId === args.memberId)
     throw new Error(
       `You cannot set yourself admin of the organization "${
         args.organizationKey
       }"`
     );
-
-  // Check that the user has access to the given organization
-  // and is an admin
-  const organizationResults = await context.db.query.organizations({
-    where: {
-      AND: [
-        { key: args.organizationKey },
-        {
-          memberRefs_some: {
-            AND: [{ auth0Id: context.userId }, { role: 'Admin' }],
-          },
-        },
-      ],
-    },
-  });
-  if (!organizationResults || organizationResults.length === 0)
-    // TODO: return proper status code
-    throw new Error(
-      `The organization with key "${
-        args.organizationKey
-      }" is either not found or you are not an Admin of such organization. In case you are part of the organization, remember that only admin members can promote members to admin.`
-    );
-
-  // Check if the `memberId` is alredy Admin
+};
+const isTargetMemberNotAnAdmin = async (args, context) => {
   const memberAdminResult = await context.db.query.memberRefs({
     where: {
       AND: [
@@ -46,18 +19,31 @@ module.exports = async (parent, args, context, info) => {
   });
   if (memberAdminResult && memberAdminResult.length === 0)
     throw new Error(
-      `The member "${args.memberId}" is not part of the organization "${
-        args.organizationKey
-      }"`
+      `The member you are trying to promote "${
+        args.memberId
+      }" is not part of the organization "${args.organizationKey}"`
     );
   if (memberAdminResult && memberAdminResult[0].role === 'Admin')
     throw new Error(
-      `The member "${
+      `The member you are trying to promote "${
         args.memberId
       }" has already the role "Admin" of the organization "${
         args.organizationKey
       }"`
     );
+};
+
+/**
+ * Only admin members can set other members as admin.
+ *
+ * Args:
+ * - organizationKey
+ * - memberId
+ */
+module.exports = async (parent, args, context, info) => {
+  isTargetMemberNotSelf(args, context);
+  await isUserAdminOfOrganization(args, context);
+  await isTargetMemberNotAnAdmin(args, context);
 
   await context.db.mutation.updateManyMemberRefs({
     where: {
