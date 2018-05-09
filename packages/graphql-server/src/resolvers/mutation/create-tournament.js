b6431745d5a15ef7301d3852a87a5c188c9e590a
Nicola Molinari <nicola.molinari@commercetools.de>
2018-05-09 09:27:58 +0200

    fix(graphql-server): creatTournament mutation data and return value

--- packages/graphql-server/src/resolvers/mutation/create-tournament.js
+++ packages/graphql-server/src/resolvers/mutation/create-tournament.js
@@ -6,6 +6,22 @@ const isTeamSizeEqualOrGreaterThanOne = teamSize => {
     throw new ValidationError('Team size must be equal or greater than 1');
 };
 
+const mapByTeamSize = (size, mapFn) => {
+  switch (size) {
+    case 'Small':
+      return Array.from(new Array(4)).map(mapFn);
+    case 'Medium':
+      return Array.from(new Array(8)).map(mapFn);
+    case 'Large':
+      return Array.from(new Array(16)).map(mapFn);
+    case 'XLarge':
+      return Array.from(new Array(32)).map(mapFn);
+    default:
+      // Ignore, we never get here
+      throw new Error(`Unexpected tournament size: ${size}`);
+  }
+};
+
 /**
  * Args:
  * - name
@@ -14,19 +30,22 @@ const isTeamSizeEqualOrGreaterThanOne = teamSize => {
  * - organizationKey
  * - teamSize
  */
-module.exports = async (parent, args, context) => {
+module.exports = async (parent, args, context, info) => {
   isTeamSizeEqualOrGreaterThanOne(args.teamSize);
   await hasUserAccessToOrganization(args, context);
 
-  return context.db.mutation.createTournament({
-    data: {
-      name: args.name,
-      size: args.size,
-      status: 'New',
-      discipline: args.discipline,
-      teamSize: args.teamSize,
-      teams: { create: [] },
-      organization: { connect: { key: args.organizationKey } },
+  return context.db.mutation.createTournament(
+    {
+      data: {
+        name: args.name,
+        size: args.size,
+        status: 'New',
+        discipline: args.discipline,
+        teamSize: args.teamSize,
+        teams: { create: mapByTeamSize(args.size, () => ({})) },
+        organization: { connect: { key: args.organizationKey } },
+      },
     },
-  });
+    info
+  );
 };
