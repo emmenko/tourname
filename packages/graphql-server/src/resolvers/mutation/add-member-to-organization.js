a32781aaaa1368a9351867e0b96275cdf00e0b92
Nicola Molinari <nicola.molinari@commercetools.de>
2018-05-10 15:39:36 +0200

    refactor(graphql-server): ensure that there is only one unique member for each org

--- packages/graphql-server/src/resolvers/mutation/add-member-to-organization.js
+++ packages/graphql-server/src/resolvers/mutation/add-member-to-organization.js
@@ -1,4 +1,5 @@
 const isUserInAuth0 = require('../../validations/is-user-in-auth0');
+const hasMemberForOrganization = require('../../validations/has-member-for-organization');
 
 /**
  * Only admin members can add other members to the organization
@@ -9,6 +10,7 @@ const isUserInAuth0 = require('../../validations/is-user-in-auth0');
  */
 module.exports = async (parent, args, context, info) => {
   await isUserInAuth0(args, context);
+  await hasMemberForOrganization(args, context);
 
   return context.db.mutation.updateOrganization(
     {
