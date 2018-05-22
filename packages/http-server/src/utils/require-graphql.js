4081d824f309dcf86df0f54328b89cc12e3b1b93
Nicola Molinari <nicola.molinari@commercetools.de>
2018-05-22 13:03:34 +0200

    chore: remove http-server package

--- packages/http-server/src/utils/require-graphql.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { readFileSync } = require('fs');
-
-// At the moment there is no proper way of loading `.graphql` or `.gql` files
-// in nodejs. This workaround basically uses `readFileSync` to read the file
-// content as a string.
-// https://github.com/apollographql/graphql-tools/issues/273
-module.exports = folderPath => filePath =>
-  readFileSync(path.join(folderPath, filePath), 'utf-8');
