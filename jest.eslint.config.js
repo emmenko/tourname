c4fe6c5aa7c66352762dae0d73b1fbfa3fb9e923
Nicola Molinari <nicola.molinari@commercetools.de>
2018-06-09 09:50:51 +0200

    chore: upgrade deps

--- jest.eslint.config.js
+++ jest.eslint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   runner: 'jest-runner-eslint',
   displayName: 'eslint',
+  moduleFileExtensions: ['js'],
   testMatch: ['<rootDir>/packages/**/*.js'],
+  testPathIgnorePatterns: ['node_modules', 'build', 'lib', 'dist'],
+  watchPlugins: ['jest-plugin-filename'],
 };
